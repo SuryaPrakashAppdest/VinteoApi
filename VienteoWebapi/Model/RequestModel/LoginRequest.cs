2787f009c37853fe7cf4c1ed33e75f54ba2eb372
Surya <surya.jagarapu@sumtotalsystems.com>
2018-09-03 12:11:12 +0530

    Add Model

---
+++ VienteoWebapi/Model/RequestModel/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Model.RequestModel
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ VienteoWebapi/Model/ResponseModel/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Model.ResponseModel
+{
+    public class LoginResponse
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string EmailId { get; set; }
+        public string MobileNo { get; set; }
+    }
+}
